152a0eab9e4e60c6504b12a18e2257a2cadc20fb
aasurjya <ahandique8@gmail.com>
2023-08-25 14:14:54 +0000

    Readme

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
+import express from "express";
+import bookRoutes from "./routes/api";
+const app = express();
+app.use(express.json());
+app.use('api', bookRoutes);
+
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log("Server is running" + PORT);
+    
+})
 console.log("App is running on the port")
